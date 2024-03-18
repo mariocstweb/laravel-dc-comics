0f933a684e0016fdbbd912be17da4dd254ad3f29
mariocstweb <mariocstmra@gmail.com>
2024-03-18 15:11:03 +0100

    added route for delete

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.sh
 Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 
 Route::put('/comics/{comic}', [ComicController::class, 'update'])->name('comics.update');
+
+Route::delete('/comics{comic}', [ComicController::class, 'destroy'])->name('comics.destroy')
