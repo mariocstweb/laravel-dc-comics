340534ac0546e78a0690767fdec2f8959d10fd4f
mariocstweb <mariocstmra@gmail.com>
2024-03-18 18:03:50 +0100

    added route for trash

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,26 @@ Route::get('/', function () {
     return view('layouts.main');
 });
 
+// Rotta per spostare un fumetto nel cestino
+Route::get('/comics/trash', [ComicController::class, 'trash'])->name('comics.trash')->withTrashed();
+
+// Rotta per il restore di un fumetto
+Route::patch('/comics/{comic}/restore', [ComicController::class, 'restore'])->name('comics.restore')->withTrashed();
+
+// Rotta per eliminare un fumetto definitivamente
+Route::delete('/comics/{comic}/drop', [ComicController::class, 'drop'])->name('comics.drop')->withTrashed();
+
+
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
-
-Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
-
-Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
-
-Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
 
 Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 
-Route::put('/comics/{comic}', [ComicController::class, 'update'])->name('comics.update');
+Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 
-Route::delete('/comics{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
+Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show')->withTrashed();
+
+Route::put('/comics/{comic}', [ComicController::class, 'update'])->name('comics.update')->withTrashed();
+
+Route::delete('/comics/{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
+
+Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit')->withTrashed();
